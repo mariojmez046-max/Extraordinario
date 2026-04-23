256481661667c79c229c93d18b746c8b5b8d4956
mariojmez046-max <mariojmez046@gmail.com>
2026-04-23 11:10:09 -0500

    Refactorizacion app.py

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/productos')
-def productos():
-    import mysql.connector
+import mysql.connector
     mydb = mysql.connector.connect(
         host="46.28.42.226",
         user="u760464709_24005367_usr",
         password="Q4Q?ZfPc+k",
         database="u760464709_24005367_bd"
-    )
 
+@app.route('/productos')
+def productos():
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM productos")
     myresult = mycursor.fetchall()
@@ -22,14 +21,6 @@ def productos():
 
 @app.post('/producto')
 def producto():
-    import mysql.connector
-    mydb = mysql.connector.connect(
-        host="46.28.42.226",
-        user="u760464709_prueba_usr",
-        password="|Au/mc*H2jH3",
-        database="u760464709_prueba_bd"
-    )
-
     mycursor = mydb.cursor()
     sql = "INSERT INTO productos (nombre, categoria, precio, existencias) VALUES (%s, %s, %s, %s)"
     val = (request.form['txtNombre'], request.form['cboCategoria'], request.form['txtPrecio'], request.form['txtExistencias'])
