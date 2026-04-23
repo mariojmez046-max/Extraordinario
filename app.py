8ad636c28a52e87f458f4337e2b465e26136bbf9
mariojmez046-max <mariojmez046@gmail.com>
2026-04-23 11:44:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/Clientes')
+def Clientes():
+    import mysql.connector
 import mysql.connector
     mydb = mysql.connector.connect(
         host="46.28.42.226",
         user="u760464709_24005367_usr",
         password="Q4Q?ZfPc+k",
         database="u760464709_24005367_bd"
+    )
 
-@app.route('/Usuarios')
-def usuarios():
+@app.route('/Clientes')
+def cliente():
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM Usuarios")
+    mycursor.execute("SELECT * FROM Cliente")
     myresult = mycursor.fetchall()
     return make_response(jsonify(myresult))
 
