67399f9b1a92fe75b8a0838c7dfce7d73de15f55
mariojmez046-max <mariojmez046@gmail.com>
2026-04-23 11:51:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def Usuarios():
 
 @app.post('/Usuario')
 def usuario():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_24005367_usr",
+        password="Q4Q?ZfPc+k",
+        database="u760464709_24005367_bd"
+    )
     mycursor = mydb.cursor()
     sql = "INSERT INTO Usuarios (correo, contrasena, nombre) VALUES (%s, %s, %s)"
     val = (request.form['txtNombre'], request.form['cboCategoria'], request.form['txtPrecio'], request.form['txtExistencias'])
